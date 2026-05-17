4c2f2be3c2eec609566a69074d74c8fa2e426aac
nam-joshi <namrata.joshi1998@gmail.com>
2026-05-17 12:59:34 -0700

    Remove Amazon search tests

--- cypress/e2e/amazon.cy.js
+++ cypress/e2e/amazon.cy.js
@@ -16,26 +16,6 @@ describe("Amazon Homepage", () => {
   });
 });
 
-describe("Amazon Search", () => {
-  beforeEach(() => {
-    cy.visit("https://www.amazon.com/");
-  });
 
-  it("should return results for a valid search term", () => {
-    cy.get("#twotabsearchtextbox").type("laptop");
-    cy.get("#nav-search-submit-button").click();
-    cy.get("[data-component-type='s-search-result']").should("have.length.greaterThan", 0);
-  });
-
-  it("should show search suggestions while typing", () => {
-    cy.get("#twotabsearchtextbox").type("head");
-    cy.get(".s-suggestion-container, .autocomplete-results-container").should("be.visible");
-  });
-
-  it("should handle an empty search gracefully", () => {
-    cy.get("#nav-search-submit-button").click();
-    cy.url().should("include", "amazon.com");
-  });
-});
 
 
